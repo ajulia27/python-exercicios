5f057dbe2f4b188ad56d17b525203e59110466de
ajulia27 <167909420+ajulia27@users.noreply.github.com>
2024-05-07 11:03:34 -0300

    exercicio1

---
+++ entrada_saida/exercicio_1.py
@@ -0,0 +1,4 @@
+numero1 = int(input('digite o primeiro numero: '))
+numero2 = int(input('digite o segundo numero: '))
+print(f"a soma dos numeros é {numero1+numero2}")
+print(f"o produto dos numeros é {numero1*numero2}")
