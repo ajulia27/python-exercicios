993bec608ccf646ec6b197d3e5a49a8e00bf736f
ajulia27 <167909420+ajulia27@users.noreply.github.com>
2024-06-04 11:45:49 -0300

    criação exercicio_1.py

---
+++ entrada_saida/funcoes/exercicio_1 copy.py
@@ -0,0 +1,18 @@
+def CalcularMedia(nota1,nota2,nota3,nota4):
+    media = (nota1+nota2+nota3+nota4)/4
+    return media
+
+def resultado(media):
+    if (media >= 5):
+        return 'Aprovado'
+    else:
+        return 'Reprovado'
+    
+print('Calculadora de media')
+n1 = float(input('digite a 1 nota: '))
+n2 = float(input('digite a 2 nota: '))
+n3 = float(input('digite a 3 nota: '))
+n4 = float(input('digite a 4 nota: '))
+m = calcularMedia(n1,n2,n3,n4)
+r = resultado(m)
+print(f"sua media é {m}, e voce foi {r}")
